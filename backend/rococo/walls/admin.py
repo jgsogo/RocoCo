990843708f7b663bad6c9569951cb39d56fe2d99
Javier G. Sogo <jgsogo@gmail.com>
2019-09-07 17:34:33 +0200

    register to admin

--- backend/rococo/walls/admin.py
+++ backend/rococo/walls/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import Area, Sector, Route, Degree, Repetition
+from .models import Area, Sector, Route, Degree, Repetition, Presa, PresaCatch, Seguro
 
 admin.site.register(Area)
 admin.site.register(Sector)
 admin.site.register(Route)
 admin.site.register(Degree)
 admin.site.register(Repetition)
+admin.site.register(Presa)
+admin.site.register(PresaCatch)
+admin.site.register(Seguro)
